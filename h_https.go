862ab30a8d628c0f78e9d8ccbbee53abd4d20bfc
noypi <adrian.migraso@gmail.com>
2018-01-28 06:12:46 +0800

    .

--- h_https.go
+++ h_https.go
@@ -2,18 +2,23 @@ package webutil
 
 import (
 	"net/http"
-	"strings"
 
 	"github.com/noypi/router"
 )
 
-func EnsureHTTPS(redirect string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if !strings.HasPrefix(r.Proto, "HTTPS") {
-			c := router.ContextW(w)
-			http.Redirect(w, r, redirect, http.StatusTemporaryRedirect)
-			c.Abort()
-			return
-		}
+func EnsureHTTPS(w http.ResponseWriter, r *http.Request) {
+	if "127.0.0.1" == r.RemoteAddr {
+		return
+	}
+	if "true" == r.Header.Get("X-Appengine-Cron") ||
+		0 < len(r.Header.Get("X-AppEngine-QueueName")) {
+		return
+	}
+	if r.URL.Scheme != "https" {
+		c := router.ContextW(w)
+		urlpath := "https://" + r.URL.Host + r.URL.Path
+		http.Redirect(w, r, urlpath, http.StatusTemporaryRedirect)
+		c.Abort()
+		return
 	}
 }
